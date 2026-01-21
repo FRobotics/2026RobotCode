c7ed77a225899118e1f4d95fa037bbd755cea059
FRobotics4150 <33380251+FRobotics4150@users.noreply.github.com>
2026-01-20 20:26:20 -0500

    Added Network Table variables for Pitch, Yaw, Roll,, and Target Speed.

--- 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/SwerveDrive.java
+++ 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/SwerveDrive.java
@@ -1,5 +1,11 @@
 package frc.robot;
 
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+import java.util.function.BooleanSupplier;
+
+import Lib4150.Lib4150NetTableSystemSend;
+
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
@@ -20,6 +26,7 @@ public class SwerveDrive {
     static private double gyroRollDeg = 0.0;
     static private double gyroPitchDeg = 0.0;
     static private boolean gryoConnected = false;
+    static private Lib4150NetTableSystemSend sender = new Lib4150NetTableSystemSend("Swerve Drive");
 
 
     private SwerveDrive (){
@@ -40,11 +47,27 @@ public class SwerveDrive {
             module4.getModuleLocation()  );
 
         //init gyro
-        swerveGyro = new ADIS16470_IMU(ADIS16470_IMU.IMUAxis.kX, ADIS16470_IMU.IMUAxis.kY , ADIS16470_IMU.IMUAxis.kZ, SPI.Port.kOnboardCS0, ADIS16470_IMU.CalibrationTime._8s );
+        // original values, roll=kx, yaw=ky, pitch=kz
+        swerveGyro = new ADIS16470_IMU(ADIS16470_IMU.IMUAxis.kZ, ADIS16470_IMU.IMUAxis.kX, ADIS16470_IMU.IMUAxis.kY, SPI.Port.kOnboardCS0, ADIS16470_IMU.CalibrationTime._8s );
         gyroRollDeg = swerveGyro.getAngle(ADIS16470_IMU.IMUAxis.kRoll);
         gyroYawDeg  = swerveGyro.getAngle(ADIS16470_IMU.IMUAxis.kYaw);
         gyroPitchDeg = swerveGyro.getAngle(ADIS16470_IMU.IMUAxis.kPitch);
         gryoConnected = swerveGyro.isConnected();
+
+        // Network Table
+        DoubleSupplier gyroRoll = () -> swerveGyro.getAngle(ADIS16470_IMU.IMUAxis.kRoll);
+        DoubleSupplier gyroYaw = () -> swerveGyro.getAngle(ADIS16470_IMU.IMUAxis.kYaw);
+        DoubleSupplier gyroPitch = () -> swerveGyro.getAngle(ADIS16470_IMU.IMUAxis.kPitch);
+        BooleanSupplier gyroConnect = () -> swerveGyro.isConnected();
+        Supplier<String> speedTarget = () -> locSpeedTarget.toString();
+
+        //Pitch and roll are swapped
+        sender.addItemDouble("Pitch", gyroPitch);
+        sender.addItemDouble("Roll", gyroRoll);
+        sender.addItemDouble("Yaw", gyroYaw);
+        sender.addItemBoolean("Connected", gyroConnect);
+        sender.addItemString("Speed Target", speedTarget);
+
         
         
     }
@@ -59,6 +82,9 @@ public class SwerveDrive {
 
     static public double getRoll(){
         return gyroRollDeg;
+    }
+    static public ChassisSpeeds getTargetSpeed(){
+        return locSpeedTarget;
     }
 
     static public void SwerveExec(double timeValue){
@@ -75,6 +101,8 @@ public class SwerveDrive {
         module3.ExecuteLogic( desiredModStates[2], timeValue);
         module4.ExecuteLogic( desiredModStates[3], timeValue);
 
+        //update network tables
+        sender.triggerUpdate();
 
     }
     

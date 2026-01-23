93febe8aa152acf530c0cc1e61eec01758579258
FRobotics4150 <33380251+FRobotics4150@users.noreply.github.com>
2026-01-22 20:56:45 -0500

    began odometry still needs update method

--- 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/SwerveOdometry.java
+++ 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/SwerveOdometry.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.wpilibj.Timer;
 
 
+
 public class SwerveOdometry {
 
     
@@ -16,15 +17,12 @@ public class SwerveOdometry {
     static private double xpos = 0.0;
     static private double ypos = 0.0;
     static private double rotpos = 0.0;
+    static private SwerveDriveOdometry swerveOdometry;
 
     static void init(){
         Pose2d initPose = new Pose2d(0.0,0.0,new Rotation2d(0.0));
-        locPoseEst = new SwerveDrivePoseEstimator(
-                        SwerveDrive.publicDriveKinematics, 
-                        new Rotation2d(SwerveDrive.getYaw()), 
-                        null, 
-                        initPose );
-
+        locPoseEst = new SwerveDrivePoseEstimator( SwerveDrive.publicDriveKinematics, new Rotation2d(SwerveDrive.getYaw()),  null, initPose );
+        swerveOdometry = new SwerveDriveOdometry(SwerveDrive.getKinematics(), new Rotation2d(SwerveDrive.getRoll()), SwerveDrive.getModulePositions(), initPose);
 
         sender.addItemDouble("X position", SwerveOdometry::getxposition);
         sender.addItemDouble("Y position", SwerveOdometry::getyposition);

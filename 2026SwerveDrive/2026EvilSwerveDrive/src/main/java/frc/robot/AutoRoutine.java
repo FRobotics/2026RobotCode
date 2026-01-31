51171a0a5f468943c0d522c024c408d6ecda9427
FRobotics4150 <33380251+FRobotics4150@users.noreply.github.com>
2026-01-31 15:56:43 -0500

    Worked on calling files

--- 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/AutoRoutine.java
+++ 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/AutoRoutine.java
@@ -1,13 +1,29 @@
 package frc.robot;
 
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException; 
+import java.util.Scanner;         
+
 
 public class AutoRoutine {
     private String locAutoDescription = "";
     private ArrayList<AutoStep> locAutoSteps = new ArrayList<AutoStep>();
+    private File myFile;
 
     public AutoRoutine(String autoDescription, AutoStep[] steps){
+        myFile = new File("" /*put file here*/);
         locAutoDescription = autoDescription;
+
+        try (Scanner myReader = new Scanner(myFile)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+      }
+    } catch (FileNotFoundException e) {
+      
+      e.printStackTrace();
+    }
 
         for (AutoStep x : steps) {
             locAutoSteps.add(x);
@@ -22,6 +38,7 @@ public class AutoRoutine {
     public AutoStep getStep(int index){
         return locAutoSteps.get(index);
     }
+
 
     // TODO: Add a getter for the description of this routine.
 

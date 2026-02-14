ad8b81b712fd99beaa3b1d908ec07b5c181c44bf
Frobotics 4150 <frobotics4150@gmail.com>
2026-02-14 11:42:34 -0500

    add todo to trajectory

--- 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/TrajectorySystem.java
+++ 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/TrajectorySystem.java
@@ -1,15 +1,22 @@
 
 package frc.robot;
 
+import java.util.Optional;
+
 import choreo.Choreo;
+import choreo.trajectory.SwerveSample;
+import choreo.trajectory.Trajectory;
 
 
 public class TrajectorySystem {
    
     private static Choreo.TrajectoryCache TrajectoryStorage;
 
+    // things for executing the trajectory
     private static Trajectory<SwerveSample> TrajectoryToRun;
+    // TODO: add starting time, add initComplete
 
+    
     private TrajectorySystem(){
 
     }
@@ -30,11 +37,15 @@ public class TrajectorySystem {
 
     }
 
+    @SuppressWarnings("unchecked")
     public static boolean FollowTrajectory(Boolean Init, String TrajectoryName){
 
-        if(Init){
-            TrajectoryToRun = TrajectoryStorage.loadTrajectory(TrajectoryName);
+        if(Init) {
+            var tryToLoadTrajectory = TrajectoryStorage.loadTrajectory(TrajectoryName);
+            if ( tryToLoadTrajectory.isPresent()) {
+                TrajectoryToRun = (Trajectory<SwerveSample>)tryToLoadTrajectory.get();
+            }
         }
+        return false;
     }
-
 }

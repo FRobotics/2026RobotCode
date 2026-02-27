0f56b70bb4d9602fdf51ab45c85bc390489e2868
FRobotics4150 <33380251+FRobotics4150@users.noreply.github.com>
2026-02-26 20:58:17 -0500

    Made Match system (other computer did not)

--- 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/MatchSystem.java
+++ 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/MatchSystem.java
@@ -1,23 +1,50 @@
 package frc.robot;
 
+import java.util.Optional;
+import Lib4150.Lib4150NetTableSystemSend;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 public class MatchSystem {
   
+    static private enum RobotCodePhase{
+        InitialStartup,
+        GyroCalibrated,
+        TrajectoriesRead,
+        AutosRead,
+        StartupComplete,
+        Disabled,
+        AutoInitComplete,
+        AutoRunning,
+        TeleopRunning;
+
+    }
+
+    private static final int SKIP_INTERVAL=10;
+    
     private static boolean areRed;
     private static boolean areBlue;
+    private static int skipCounter=0;
+    private static Lib4150NetTableSystemSend locNTSend;
+    private static RobotCodePhase locRobotCodePhase = RobotCodePhase.InitialStartup;
 
   
     private MatchSystem(){}
 
-    public static void setRed(boolean paramIsRed) {
-        areRed = paramIsRed;
-        areBlue = !paramIsRed;
-
+    private static void setRed() {
+        areRed = true;
+        areBlue = false;
+        return;
     }
-    public static void setBlue(boolean paramIsBlue)
-    {
-        areBlue = paramIsBlue;
-        areRed = !paramIsBlue;
-
+    private static void setBlue(){
+        areBlue = true;
+        areRed = false;
+        return;
+    }
+    private static void setUnknown(){
+        areBlue=false;
+        areRed=false;
+        return;
     }
     public static boolean isRed() {
         return areRed;
@@ -25,11 +52,92 @@ public class MatchSystem {
     public static boolean isBlue() {
         return areBlue;
     }
-    //TODO: what needs to go in here?
-    //currently jsut returning if we are blue or not
-    public static boolean disableExec() {
-        return areBlue;
+
+    public static void init(){
+        locNTSend = new Lib4150NetTableSystemSend("MatchSystem");
+        locNTSend.addItemBoolean("isRed", MatchSystem::isRed);
+        locNTSend.addItemBoolean("isBlue", MatchSystem::isRed);
+        locNTSend.addItemDouble("RobotCodePhase", MatchSystem::getRobotCodePhaseNumber);
+
+        locNTSend.triggerUpdate();
     }
 
     
+    //currently jsut returning if we are blue or not
+    public static void disableExec() {
+        skipCounter++;
+        if (skipCounter >= SKIP_INTERVAL){
+            skipCounter=0;
+            Optional<Alliance> ourAlliance = DriverStation.getAlliance();
+            if(ourAlliance.isPresent()){
+                switch(ourAlliance.get()){
+                    case Red:
+                        setRed();
+                        break;
+                    case Blue:
+                        setBlue();
+                        break;
+                    default:
+                        setUnknown();
+                        break;
+                }
+            }
+            else{
+                setUnknown();
+            }
+            locRobotCodePhase=RobotCodePhase.Disabled;
+            locNTSend.triggerUpdate();
+        }
+        return;
+    }
+
+    public static void setRobotPhaseInitialStartup(){
+        locRobotCodePhase = RobotCodePhase.InitialStartup;
+        locNTSend.triggerUpdate();
+        return;
+    }    
+    public static void setRobotPhaseGyroCalibrated(){
+        locRobotCodePhase = RobotCodePhase.GyroCalibrated;
+        locNTSend.triggerUpdate();
+        return;
+    }
+    public static void setRobotPhaseTrajectoriesRead(){
+        locRobotCodePhase = RobotCodePhase.TrajectoriesRead;
+        locNTSend.triggerUpdate();
+        return;
+    }
+    public static void setRobotPhaseAutosRead(){
+        locRobotCodePhase = RobotCodePhase.AutosRead;
+        locNTSend.triggerUpdate();
+        return;
+    }
+    public static void setRobotPhaseStartupComplete(){
+        locRobotCodePhase = RobotCodePhase.StartupComplete;
+        locNTSend.triggerUpdate();
+        return;
+    }
+    public static void setRobotPhaseDisabled(){
+        locRobotCodePhase = RobotCodePhase.Disabled;
+        locNTSend.triggerUpdate();
+        return;
+    }
+    public static void setRobotPhaseAutoInitComplete(){
+        locRobotCodePhase = RobotCodePhase.AutoInitComplete;
+        locNTSend.triggerUpdate();
+        return;
+    }
+    public static void setRobotPhaseAutoRunning(){
+        locRobotCodePhase = RobotCodePhase.AutoRunning;
+        locNTSend.triggerUpdate();
+        return;
+    }
+    public static void setRobotPhaseTeleopRunning(){
+        locRobotCodePhase = RobotCodePhase.TeleopRunning;
+        locNTSend.triggerUpdate();
+        return;
+    }
+    public static double getRobotCodePhaseNumber(){
+        return (double)locRobotCodePhase.ordinal();
+    }
+
 }

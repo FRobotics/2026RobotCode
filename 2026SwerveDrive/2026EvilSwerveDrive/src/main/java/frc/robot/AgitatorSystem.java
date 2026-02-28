bc65cc4f989c727e1c65dd964dbd7a20566cf00b
FRobotics4150 <33380251+FRobotics4150@users.noreply.github.com>
2026-02-28 15:55:53 -0500

    Added speed control to agitator and feeder systems

--- 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/AgitatorSystem.java
+++ 2026SwerveDrive/2026EvilSwerveDrive/src/main/java/frc/robot/AgitatorSystem.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import Lib4150.Lib4150NetTableSystemSend;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
@@ -21,11 +23,28 @@ public class AgitatorSystem {
     private static Lib4150NetTableSystemSend locNTSend;
 
     // TRUE = we want agitator to be on.  FALSE = we want agitator to be off
-    private static boolean locAgitatorOn = false; 
+    private static boolean locAgitatorOn = false;
+    private static boolean locAgitatorOnRev = false; 
     private static SparkMax AgitatorMotor;
     private static RelativeEncoder AgitatorMotorEncoder;
     private static double AgitatorOutput = 0.0;
     private static double AgitatorRPM = 0.0;
+    private static double locAgitatorSetpointRPM = 0.0;
+    private static double locAgitatorFFoutput = 0.0;
+    private static double locAgitatorPIDoutput = 0.0;
+    private static SimpleMotorFeedforward AgitatorFeedForward;
+    private static PIDController AgitatorPID;
+    private static final double Agitator_Kn = 1.0 / 5000.0;
+    private static final double Agitator_Ks = 0.0;
+    private static final double Agitator_Kv = Agitator_Kn;
+    private static final double Agitator_Ka = 0.0;
+    private static final double Agitator_Kp = Agitator_Kn * 0.0;
+    private static final double Agitator_Ki = Agitator_Kn * 0.0;
+    private static final double Agitator_Kd = Agitator_Kn * 1.0E-6;
+    private static final double Agitator_Izone = 200.0;
+    private static final double Agitator_Imax = 0.30;
+
+
 
     public static void init() {
 
@@ -33,6 +52,13 @@ public class AgitatorSystem {
         // init agitator motor
         AgitatorMotor = new SparkMax(8,MotorType.kBrushless);
         AgitatorMotorEncoder = AgitatorMotor.getEncoder();
+
+        //Speed control
+        AgitatorFeedForward = new SimpleMotorFeedforward(Agitator_Ks, Agitator_Kv, Agitator_Ka);
+        AgitatorPID = new PIDController(Agitator_Kp, Agitator_Ki, Agitator_Kd);
+        AgitatorPID.setIntegratorRange(-Agitator_Imax, Agitator_Imax);
+        AgitatorPID.setIZone(Agitator_Izone);
+
 
         // ensure agitator starts off
         cmdAgitatorOff();
@@ -56,11 +82,19 @@ public class AgitatorSystem {
         // if off, output 0
 
         if (locAgitatorOn){
-            AgitatorOutput=0.2;
+            locAgitatorSetpointRPM = 0.5 / Agitator_Kn;
+        }
+        else if ( locAgitatorOnRev){
+            locAgitatorSetpointRPM = -0.1 / Agitator_Kn;
         }
         else {
-            AgitatorOutput=0;
+            locAgitatorSetpointRPM = 0.0;
         };
+
+        // Agitator Speed Control
+        locAgitatorFFoutput = AgitatorFeedForward.calculate(locAgitatorSetpointRPM);
+        locAgitatorPIDoutput = AgitatorPID.calculate(AgitatorRPM, locAgitatorSetpointRPM);
+
 
         AgitatorMotor.set(AgitatorOutput);
 

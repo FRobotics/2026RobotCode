629df8e236e3885d5532baa25c0bef18b39e9892
FRobotics4150 <33380251+FRobotics4150@users.noreply.github.com>
2026-03-04 20:58:22 -0500

    tuning

--- 2026StartupMrS_EvilSwerveDrive/src/main/java/frc/robot/IntakeSystem.java
+++ 2026StartupMrS_EvilSwerveDrive/src/main/java/frc/robot/IntakeSystem.java
@@ -119,7 +119,7 @@ public class IntakeSystem {
 
         // ---------if we just hit the limit switch, set the value of the encoder position.
         if ( IntakeArmLowLimitSwitchEdgeOn.execEdgeOn(IntakeArmLowLimitSwitchState) ) {
-            // TODO: It needs to settle first.  Need onDelay IntakeArmMotorEncoder.setPosition( calcEncoderRawValueFromArmDeg(INTAKEDOWNLIMITSWITCHANGLE));
+            IntakeArmMotorEncoder.setPosition( calcEncoderRawValueFromArmDeg(INTAKEDOWNLIMITSWITCHANGLE));
         }
         
         // --------read the arm position in degrees.

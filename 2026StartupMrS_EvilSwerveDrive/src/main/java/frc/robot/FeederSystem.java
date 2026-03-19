d318ef3f7aedc3ea0f63d5acbdb729e11764c728
jsimpso81 <60305400+jsimpso81@users.noreply.github.com>
2026-03-19 17:32:24 -0400

    feeder pid tuning

--- 2026StartupMrS_EvilSwerveDrive/src/main/java/frc/robot/FeederSystem.java
+++ 2026StartupMrS_EvilSwerveDrive/src/main/java/frc/robot/FeederSystem.java
@@ -33,11 +33,11 @@ public class FeederSystem {
     private static final double Feeder_Ka = 0.0;
     // --------PID
     // --------Kp - proportional constant    output =  error * Kp
-    private static final double Feeder_Kp = Feeder_Kn * 2.0;    // was 0.6  
+    private static final double Feeder_Kp = Feeder_Kn * 1.2;    // was 0.6  
     // --------Ki - integral constant   output  = Ki x integral( error )
     private static final double Feeder_Ki = Feeder_Kn * 2.0;
     // --------kd = derivative constant     output = Kd * derivative( error )
-    private static final double Feeder_Kd = Feeder_Kn * 1.0E-5; 
+    private static final double Feeder_Kd = Feeder_Kn * 1.0E-6; 
     // --------integral zone ( in sp/pv units )
     // --------Izone -- Error has to be within this amount to be used.
     private static final double Feeder_Izone = 60.0;  // Error RPM where I is used.
